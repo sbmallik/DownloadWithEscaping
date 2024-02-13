19e47f7ccb94fa0d1904f4bf32e53e9b890a396b
sbmallik <smallik@gannett.com>
2024-02-13 17:11:39 -0500

    Initial Commit

---
+++ DownloadWithEscaping/DownloadWithEscapingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadWithEscapingApp.swift
+//  DownloadWithEscaping
+//
+//  Created by Mallik, Sudip on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct DownloadWithEscapingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
