92f3e369cd1f020397ab9a74ec8b68dfc25af1dd
sbmallik <smallik@gannett.com>
2024-02-15 14:12:49 -0500

    Initial commit.

---
+++ DownloadWithEscaping/UploadWithEscaping.swift
@@ -0,0 +1,18 @@
+//
+//  UploadWithEscaping.swift
+//  DownloadWithEscaping
+//
+//  Created by Mallik, Sudip on 2/13/24.
+//
+
+import SwiftUI
+
+struct UploadWithEscaping: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadWithEscaping()
+}
